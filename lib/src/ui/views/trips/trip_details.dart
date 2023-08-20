cf1b8d07c32dd7b0a514c130e79bd47c9e6b286f
toddnelson <tello_nii@outlook.com>
2023-08-20 03:38:34 +0000

    add extra data

--- lib/src/ui/views/trips/trip_details.dart
+++ lib/src/ui/views/trips/trip_details.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:mizormor/src/core/model/user_trip.dart';
 
 import '../../../../utils/extensions/padding.dart';
 import '../../widgets/table_row_element.dart';
 
-///
-class TripDetailsView extends ConsumerStatefulWidget {
-  ///
-  const TripDetailsView({super.key});
+class TripDetailView extends ConsumerWidget {
+  const TripDetailView({required this.trip, super.key});
+  final UserTrips trip;
 
   @override
-  ConsumerState createState() => _TripDetailsViewState();
-}
-
-class _TripDetailsViewState extends ConsumerState<TripDetailsView> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
     return Scaffold(
         body: CustomScrollView(
@@ -28,7 +23,7 @@ class _TripDetailsViewState extends ConsumerState<TripDetailsView> {
         SliverList.list(
           children: [
             Text(
-              'Here is an overview of the trip from Accra to Takoradi.',
+              'Here is an overview of the trip from Accra to ${trip.destination}.',
               style: theme.textTheme.bodyLarge,
             ).paddingOnly(bottom: 24),
             Stack(
@@ -54,7 +49,10 @@ class _TripDetailsViewState extends ConsumerState<TripDetailsView> {
                     borderRadius: BorderRadius.circular(16),
                   ),
                   child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-                    Image.asset('assets/images/img_1.png').paddingOnly(bottom: 24),
+                    Image.asset(
+                      'assets/images/img_1.png',
+                      height: 200,
+                    ).paddingOnly(bottom: 24),
                     Table(defaultVerticalAlignment: TableCellVerticalAlignment.middle, children: [
                       buildTableRow(
                         context: context,
@@ -70,35 +68,43 @@ class _TripDetailsViewState extends ConsumerState<TripDetailsView> {
                       ),
                       buildTableRow(
                         context: context,
+                        leading: 'Status',
+                        trailing: trip.tripStatus,
+                      ),
+                      buildTableRow(
+                        context: context,
                         leading: 'Ticket',
-                        trailing: 'TKAR123NOV2623',
+                        trailing: trip.ticketId,
                       ),
                       buildTableRow(
                         context: context,
                         leading: 'Bus number',
-                        trailing: 'GT - 4588 - 23',
+                        trailing: trip.bus,
+                      ),
+                      buildTableRow(
+                        context: context,
+                        leading: 'Pickup point',
+                        trailing: trip.pickupPoint,
                       ),
                       buildTableRow(
                         context: context,
                         leading: 'Departure date',
                         trailing: DateFormat.yMMMMd().format(
-                          DateTime.now().add(
-                            const Duration(days: 167),
-                          ),
+                          trip.departureTime,
                         ),
                       ),
                       buildTableRow(
                         context: context,
                         leading: 'Departure time',
-                        trailing: const TimeOfDay(hour: 11, minute: 00).format(context),
+                        trailing: TimeOfDay.fromDateTime(trip.departureTime).format(context),
                       ),
-                    ]).paddingOnly(bottom: 72),
+                    ]).paddingOnly(bottom: 20),
                     Text(
                       'Ticket cost',
                       style: theme.textTheme.bodyLarge?.copyWith(color: Colors.grey.shade600),
                     ).paddingOnly(bottom: 8),
                     Text(
-                      NumberFormat.currency(symbol: 'GH₵ ').format(399),
+                      NumberFormat.currency(symbol: 'GH₵ ').format(trip.ticketPrice),
                       style: theme.textTheme.bodyLarge?.copyWith(
                         fontSize: 18,
                         fontWeight: FontWeight.w700,
@@ -107,7 +113,7 @@ class _TripDetailsViewState extends ConsumerState<TripDetailsView> {
                   ]),
                 ),
                 Positioned(
-                  bottom: 120,
+                  bottom: 72,
                   left: 0,
                   right: 0,
                   child: Row(
@@ -122,7 +128,7 @@ class _TripDetailsViewState extends ConsumerState<TripDetailsView> {
                         ),
                       ),
                       ...List.generate(15,
-                          (index) => Container(color: theme.colorScheme.primary.withOpacity(0.2), height: 1, width: 5)),
+                          (index) => Container(color: theme.colorScheme.primary.withOpacity(0.4), height: 1, width: 5)),
                       Container(
                         width: 30,
                         height: 50,
